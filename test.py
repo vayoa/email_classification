ca6ce212b880a0f161e3bcfd9aacc86cf337fcb0
vayoa <ew0nd3r123@gmail.com>
2025-09-04 18:49:56 +0300

    Cleaned up a bit

--- test.py
+++
@@ -1,6 +0,0 @@
-import torch, platform
-
-print("is_available:", torch.cuda.is_available())
-print("device_count:", torch.cuda.device_count())
-print("name:", torch.cuda.get_device_name(0) if torch.cuda.is_available() else None)
-print("torch:", torch.__version__, "cuda:", torch.version.cuda)
